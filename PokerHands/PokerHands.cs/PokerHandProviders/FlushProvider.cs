7b0546ebf7027f467401ee802e765c062f03a5dd
atownart <atownart@gmail.com>
2016-01-03 14:58:26 +0000

    adds flush provider

---
+++ PokerHands/PokerHands.cs/PokerHandProviders/FlushProvider.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using PokerHands.cs.Entities;
+
+namespace PokerHands.cs.PokerHandProviders
+{
+    public class FlushProvider : PokerHandProviderBase, IPokerHandProvider
+    {
+        public bool HasPokerHand(IEnumerable<Card> cards)
+        {
+            var countSuiteGroups = from card in cards
+                group card by card.Suite
+                into suiteGroup
+                select suiteGroup.Count();
+            return countSuiteGroups.Count() == 1;
+        }
+    }
+}
---
+++ PokerHands/PokerHandsTests/PokerHandProviders/FlushProviderTests.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections.Generic;
+using NUnit.Framework;
+using PokerHands.cs.Entities;
+using PokerHands.cs.PokerHandProviders;
+
+namespace PokerHandsTests.PokerHandProviders
+{
+    [TestFixture]
+    public class FlushProviderTests
+    {
+        [Test]
+        public void HasPokerHand_WithAllSameSuit_HasPokerHand()
+        {
+            //Arrange 
+            var cards = new List<Card>();
+            const string suite = "S";
+            for (var i = 3; i <= 7; i++)
+            {
+                cards.Add(new Card(i.ToString(), suite, 0));
+            }
+            var flushProvider = new FlushProvider();
+
+            //Act
+            var hasHand = flushProvider.HasPokerHand(cards);
+
+            //Assert
+            Assert.That(hasHand,Is.True);
+        }
+
+        [Test]
+        public void HasPokerHand_CardsHaveDifferentSuites_DoesNotHavePokerHand()
+        {
+            //Arrange 
+            var cards = new List<Card>();
+            const string suite = "S";
+            for (var i = 3; i <= 6; i++)
+            {
+                cards.Add(new Card(i.ToString(), suite, 0));
+            }
+            cards.Add(new Card(string.Empty,"C",0));
+            var flushProvider = new FlushProvider();
+
+            //Act
+            var hasHand = flushProvider.HasPokerHand(cards);
+
+            //Assert
+            Assert.That(hasHand, Is.False);
+        }
+    }
+}
